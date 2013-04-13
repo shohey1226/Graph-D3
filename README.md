1a98dea29dde821f079e7a1b732261101bc6ee83
Shohei Kameda <shoheik@cpan.org>
2013-04-13 11:17:23 +0900

    initial release

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Graph::D3 - Blah blah blah
+
+# SYNOPSIS
+
+    use Graph::D3;
+
+# DESCRIPTION
+
+Graph::D3 is
+
+# AUTHOR
+
+Shohei Kameda <shoheik@cpan.org>
+
+# COPYRIGHT
+
+Copyright 2013- Shohei Kameda
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Graph/D3.pm
@@ -0,0 +1,39 @@
+package Graph::D3;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Graph::D3 - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Graph::D3;
+
+=head1 DESCRIPTION
+
+Graph::D3 is
+
+=head1 AUTHOR
+
+Shohei Kameda E<lt>shoheik@cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2013- Shohei Kameda
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
